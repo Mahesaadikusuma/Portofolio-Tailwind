8e2688055c34809225383f7fc51fd624aedd564a
Mahesaadikusuma <mahesaadikuzuma@gmail.com>
2023-05-03 20:19:09 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
   content: ["./src/**/*.{html,js}"],
   theme: {
     container: {
